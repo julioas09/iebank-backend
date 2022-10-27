ba0f8acb9703c7521ff9351e06b3d01a1e7c5cc9
Julio Seijas <jus@microsoft.com>
2022-10-27 16:12:03 +0200

    initial content

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pa$$w0rd1990@localhost/iebank'
+db = SQLAlchemy(app)
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    account_number = db.Column(db.String(20), nullable=False, unique=True)
+    balance = db.Column(db.Float, nullable=False, default = 0.0)
+    currency = db.Column(db.String(1), nullable=False, default="€")
+    status = db.Column(db.String(10), nullable=False, default="Active")
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Event %r>' % self.account_number
+
+    def __init__(self, name, currency):
+        self.name = name
+        self.account_number = ''.join(random.choices(string.digits, k=20))
+        self.currency = currency
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
