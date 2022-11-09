c5a8d8e1780b67d2d1272033559004f6a24bfad9
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2022-11-09 16:03:21 +0100

    Last commit

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -29,4 +29,31 @@ def test_create_account(testing_client):
     response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
     assert response.status_code == 200
 
+def test_get_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/accounts/1')
+    assert response.status_code == 200
+
+def test_update_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is updated (PUT)
+    THEN check the response is valid
+    """
+    response = testing_client.put('/accounts/1', json={'name': 'John Doe', 'currency': '€'})
+    assert response.status_code == 200
+
+def test_delete_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is deleted (DELETE)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/accounts/1')
+    assert response.status_code == 200
+
 
