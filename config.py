ff05fe19c66a0ac7bb7e38c3690884cf0997dc49
Julio Seijas <jus@microsoft.com>
2022-11-23 17:50:32 +0100

    updated donfig

--- config.py
+++ config.py
@@ -6,7 +6,14 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.getenv('DBUSER'),
+    dbpass=os.getenv('DBPASS'),
+    dbhost=os.getenv('DBHOST') + ".postgres.database.azure.com",
+    dbname=os.getenv('DBNAME')
+    )
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
@@ -15,6 +22,10 @@ class DevelopmentConfig(Config):
     dbhost=os.getenv('DBHOST'),
     dbname=os.getenv('DBNAME')
     )
-class GithubCIConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlitefile.db'
     DEBUG = True
+
+class GithubCIConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    DEBUG = True
+
+
