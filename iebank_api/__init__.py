9abfe5e09e26f2b8bbf3130a8507860e6cc6ea7e
Julio Seijas <jus@microsoft.com>
2022-11-23 17:41:04 +0100

    test ENV load

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
-
 app = Flask(__name__)
-
 load_dotenv()
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'dev':
@@ -17,10 +15,7 @@ elif os.getenv('ENV') == 'ghci':
 else:
     print("Running in production mode")
     app.config.from_object('config.ProductionConfig')
-
-
 db = SQLAlchemy(app)
-
 from iebank_api.models import Account
 db.create_all()
 CORS(app)
