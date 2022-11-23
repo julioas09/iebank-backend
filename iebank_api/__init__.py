080e5fa3e457d551d30dc4fe97a15b1b562ea401
Julio Seijas <jus@microsoft.com>
2022-11-23 17:44:52 +0100

    test ENV loading

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -9,13 +9,11 @@ load_dotenv()
 if os.getenv('ENV') == 'dev':
     print("Running in development mode")
     app.config.from_object('config.DevelopmentConfig')
-elif os.getenv('ENV') == 'prod':
-    print("Running in production mode")
-    app.config.from_object('config.ProductionConfig')
+
 else:
     print("Running in Github mode")
     app.config.from_object('config.GithubCIConfig')
-    
+
 db = SQLAlchemy(app)
 from iebank_api.models import Account
 db.create_all()
