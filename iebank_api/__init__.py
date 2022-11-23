90c77cbe2365932c74abee341e70ae0e08f878ea
Julio Seijas <jus@microsoft.com>
2022-11-23 17:47:43 +0100

    test without prod config

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -3,18 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+
 app = Flask(__name__)
+
 load_dotenv()
+
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'dev':
     print("Running in development mode")
     app.config.from_object('config.DevelopmentConfig')
-
-else:
-    print("Running in Github mode")
+elif os.getenv('ENV') == 'ghci':
+    print("Running in github mode")
     app.config.from_object('config.GithubCIConfig')
+else:
+    print("Running in production mode")
+    app.config.from_object('config.ProductionConfig')
+
 
 db = SQLAlchemy(app)
+
 from iebank_api.models import Account
 db.create_all()
 CORS(app)
